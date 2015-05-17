f06aea9b9f03e01c5b9c49dede828b7bfffd9953
ragnaroog <ragnaryok.po@mail.ru>
2015-05-17 19:12:29 +0300

    Update gotoxy.cpp

--- C++/Projects/KNB/gotoxy.cpp
+++ C++/Projects/KNB/gotoxy.cpp
@@ -3,7 +3,7 @@
 HANDLE hConsole;
 HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-void GotoXY(int X, int Y)         ///функция для Пермещения курсора
+void GotoXY(int X, int Y)         ///РџРµСЂРµРјРµС‰РµРЅРёРµ РєСѓСЂСЃРѕСЂР°
 {
 	COORD coord = { X, Y };
 	SetConsoleCursorPosition(hStdOut, coord);
